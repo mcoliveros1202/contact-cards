0db5e8e674db5bab2b9b18909c40e49149b1343d
moliveros <mcoliveros1202@gmail.com>
2022-09-09 13:40:38 -0500

    created manifest.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
 
 module.exports = {
     // mode: "development",
@@ -12,7 +13,28 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './index.html',
             title: 'Webpack Plugin',
-        })
+        }),
+        new WebpackPwaManifest({
+            name: 'Contact Cards Application',
+            short_name: 'Contact Cards',
+            description: 'Keep track of contacts!',
+            background_color: '#7eb4e2',
+            start_url: './',
+            publicPath: './',
+            icons: [
+                {
+                    src: path.resolve('src/images/icon-manifest.png'),
+                    sizes: [96, 128, 192, 256, 384, 512],
+                    destination: path.join('assets', 'icons'),
+                },
+                {
+                    src: path.resolve('src/images/icon-manifest.png'),
+                    size: '1024x1024',
+                    destination: path.join('assets', 'icons'),
+                    purpose: 'maskable'
+                }
+            ],
+        }),
     ],
     module: {
         rules: [
